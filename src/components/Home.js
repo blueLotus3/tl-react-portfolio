a70b273974d3e178c8a112553af67a5c4dfc8c84
Troy Lemons Jr <troylemonsjr@gmail.com>
2021-03-14 18:01:41 -0700

    got my basic boilerplate of react project with route and switchs installed

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Home = () => {
+    return(
+        <div className="Home-Div">
+            <h1>This Is the Home Page</h1>
+        </div>
+    )
+}
+export default Home
